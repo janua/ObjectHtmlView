0e4b7f84b75230fb6254c9ae1c186f1fa981c637
Francis <frandevelopment@gmail.com>
2011-08-27 22:22:19 +0100

    First Commit; 16 basic tests

---
+++ src/objecthtmlview.py
@@ -0,0 +1,65 @@
+'''
+Created on 1 Feb 2011
+
+@author: Francis
+'''
+import wx
+from reportlab.lib.validators import isCallable
+
+class ObjectHtmlView(wx.SimpleHtmlListBox):    
+    
+    def __init__(self, parent):
+        self.object_list = []
+        self.renderer = None
+        wx.SimpleHtmlListBox.__init__(self, parent)
+    
+    def AddObject(self, obj):
+        self.object_list.append(obj)
+    
+    def AddObjects(self, obj_list):
+        for obj in obj_list:
+            self.AddObject(obj)
+    
+    def RemoveObject(self, obj):
+        self.object_list.remove(obj)
+
+    def GetObjectCount(self):
+        return len(self.object_list)
+    
+    def GetObjectAt(self, index):
+        try:
+            return self.object_list[index]
+        except IndexError:
+            raise IndexError('That index does not exist.')
+    
+    def GetObjects(self):
+        return self.object_list
+
+    def SetRenderer(self, renderer):
+        if not isCallable(renderer):
+            raise TypeError('This object is not callable.')
+        else:
+            self.renderer = renderer
+    
+    def GetRenderer(self):
+        return self.renderer
+    
+    def RefreshObjects(self):
+        self.Clear()
+        for obj in self.object_list:
+            rendered = self.renderer(obj)
+            self.Insert(rendered, self.Count, obj)
+            
+    def SelectObject(self, obj):
+        for number in range(self.Count):
+            if self.GetClientData(number) is obj:
+                self.SetSelection(number)
+                return True
+        raise ValueError('This object does not exist')
+    
+    def GetSelectedObject(self):
+        selection = self.Selection
+        if selection > -1:
+            return self.GetClientData(selection)
+        return None
+        
---
+++ src/test_objecthtmlview.py
@@ -0,0 +1,134 @@
+'''
+Created on 1 Feb 2011
+
+@author: Francis
+'''
+import unittest
+from objecthtmlview import ObjectHtmlView
+import wx
+
+OBJECTS = ({1:2}, {3:4})
+
+class TestObjectHtmlViewAddRemove(unittest.TestCase):
+    
+    def setUp(self):
+        app = wx.PySimpleApp()
+        frame = wx.Frame(None)
+        self.ohl = ObjectHtmlView(frame)
+        frame.Show()
+            
+    def test_empty(self):
+        self.assertEqual(0, self.ohl.GetObjectCount())
+    
+    def test_add_object(self):
+        self.ohl.AddObject({'an':'object'})
+        self.assertEqual(1, self.ohl.GetObjectCount())
+    
+    def test_add_objects(self):
+        self.ohl.AddObjects(OBJECTS)
+        self.assertEqual(len(OBJECTS), self.ohl.GetObjectCount())
+    
+    def test_remove_object(self):
+        o = {'an':'object'}
+        self.ohl.AddObject(o)
+        self.assertEqual(1, self.ohl.GetObjectCount())
+        self.ohl.RemoveObject(o)
+        self.assertEqual(0, self.ohl.GetObjectCount())
+        
+    def test_remove_non_existing_object(self):
+        self.assertRaises(ValueError, self.ohl.RemoveObject, {})
+
+class TestObjectHtmlViewOrder(unittest.TestCase):
+    
+    def setUp(self):
+        app = wx.PySimpleApp()
+        frame = wx.Frame(None)
+        self.ohl = ObjectHtmlView(frame)
+        frame.Show()
+    
+    def test_first_position(self):
+        o = {'an':'object'}
+        self.ohl.AddObject(o)
+        self.assertEqual(o, self.ohl.GetObjectAt(0))
+    
+    def test_second_position(self):
+        o, o2 = {'object':'1'}, {'object':'2'}
+        self.ohl.AddObjects((o, o2))
+        self.assertEqual(o2, self.ohl.GetObjectAt(1))
+    
+    def test_wrong_index(self):
+        self.assertRaises(IndexError, self.ohl.GetObjectAt, 0)
+
+    def test_get_objects(self):
+        o, o2 = {'object':'1'}, {'object':'2'}
+        self.ohl.AddObjects((o, o2))
+        self.assertTrue(o in self.ohl.GetObjects())
+        self.assertTrue(o2 in self.ohl.GetObjects())
+
+class TestObjectHtmlViewRendering(unittest.TestCase):
+    
+    def setUp(self):
+        app = wx.PySimpleApp()
+        frame = wx.Frame(None)
+        self.ohl = ObjectHtmlView(frame)
+        frame.Show()
+        
+    def test_set_non_callable(self):
+        self.assertRaises(TypeError, self.ohl.SetRenderer, {})
+        
+    def test_set_callable(self):
+        def callable(args):
+            pass
+        self.ohl.SetRenderer(callable)
+        self.assertEqual(self.ohl.GetRenderer(), callable)
+        
+class TestObjectHtmlViewSelection(unittest.TestCase):
+    
+    def setUp(self):
+        app = wx.PySimpleApp()
+        frame = wx.Frame(None)
+        self.ohl = ObjectHtmlView(frame)
+        frame.Show()
+        
+        def render(obj):
+            return '%s' % obj
+        
+        self.ohl.SetRenderer(render)
+    
+    def test_no_selection(self):
+        self.assertEqual(None, self.ohl.GetSelectedObject())
+        
+    def test_select_non_existant_object(self):
+        self.assertRaises(ValueError, self.ohl.SelectObject, {})
+    
+    def test_select_object_one(self):
+        o = {'an':'object'}
+        self.ohl.AddObject(o)
+        self.ohl.RefreshObjects()
+        self.ohl.SelectObject(o)
+        self.assertEqual(o, self.ohl.GetSelectedObject())
+    
+    def test_select_object_two(self):
+        o, o2 = {'an':'object'}, {'object':'2'}
+        self.ohl.AddObjects((o, o2))
+        self.ohl.RefreshObjects()
+        self.ohl.SelectObject(o)
+        self.assertEqual(o, self.ohl.GetSelectedObject())
+        self.ohl.SelectObject(o2)
+        self.assertEqual(o2, self.ohl.GetSelectedObject())
+    
+    def test_select_object_three(self):
+        o, o2, o3 = {'an':'object'}, {'object':'2'}, {'o':'2'}
+        self.ohl.AddObjects((o, o2, o3))
+        self.ohl.RefreshObjects()
+        self.ohl.SelectObject(o)
+        self.assertEqual(o, self.ohl.GetSelectedObject())
+        self.ohl.SelectObject(o2)
+        self.assertEqual(o2, self.ohl.GetSelectedObject())
+        self.ohl.SelectObject(o3)
+        self.assertEqual(o3, self.ohl.GetSelectedObject())
+
+if __name__ == '__main__':
+    
+    unittest.main()
+        
